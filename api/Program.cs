10c2744f7e2e781f6448b8087340106400742e3f
kietpt2003 <tuankiet29012003@gmail.com>
2024-08-12 21:30:52 +0700

    fix bug

--- api/Program.cs
+++ api/Program.cs
@@ -91,11 +91,13 @@ builder.Services.AddScoped<IPortfolioRepository, PortfolioRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
